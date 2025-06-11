0ef19e5a6ff6820d089e78c081dff70aef4ae8bd
Enzoss23 <enzoss23@gmail.com>
2025-06-11 17:05:15 -0300

    feat: tests with typescript

--- srv/main.ts
+++ srv/main.ts
@@ -1,1 +1,15 @@
+import { Customer, Customers } from '@models/sales';
+
+const customer: Customer = {
+    email: 'teste@teste.com',
+    firstName: 'Teste',
+    lastName: 'Souza teste',
+    id: '1234'
+};
+
+const customers: Customers = [
+    customer
+];
+
+
 const funcao = (variavel: string) => console.log(variavel);
