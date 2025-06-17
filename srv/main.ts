81c7c3e6104f96221cf5173235d1c4f118916c62
Enzoss23 <enzoss23@gmail.com>
2025-06-16 22:45:37 -0300

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,6 @@
-import { ResultsHandler, Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import cds, { Request, Service } from '@sap/cds';
+import { Customers, Products, SalesOrderItem, SalesOrderItems } from '@models/sales';
+import { Console, log } from 'console';
 
 
 export default (service: Service) => {
@@ -8,6 +9,36 @@ export default (service: Service) => {
             if(!customer.email?.includes('@')){
                 customer.email = `${customer.email}@gmail.com`;
             }
+            console.log(customer);
         })
+    });
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        const items: SalesOrderItems = params.items;
+        if(!params.customer_id){
+            return request.reject(400, 'Customer inválido');
+        }
+        console.log(params)
+        if( !params.items || params.items?.length === 0){
+            return request.reject(400, 'Itens inválidos');
+        }
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if(!customer){
+            return request.reject(404, 'Customer não encontrado!');
+        }
+        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+        const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
+        const products: Products = await cds.run(productsQuery);
+
+        for(const item of items){
+            const dbProduct = products.find(product => product.id === item.product_id);
+            if(!dbProduct){
+                return request.reject(404, `Produto ${item.product_id} não encontrado!`);
+            }
+            if(dbProduct.stock === 0){
+                return request.reject(400, `Produto ${dbProduct.name}(${dbProduct.id}) sem estoque disponível!`);
+            }
+        }
     })
 }
